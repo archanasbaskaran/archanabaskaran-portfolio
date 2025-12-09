fcca9376ab8d9b61e232a6e801dc926866994533
archanasbaskaran <achusbaski@gmail.com>
2025-12-09 10:27:13 -0500

    Refactor app.py to improve layout and content

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ with tab1:
     st.subheader("BIS Health Informatics (Georgia State) | MS Data Analytics (Georgia Tech)")
     st.write("Atlanta, GA | achusbaski@gmail.com | [LinkedIn](https://www.linkedin.com/in/archana-baskaran-2a605517b/)")
   
-    col1, col2 = st.columns([2, 1])
+    col1, col2 = st.columns([1, 2])
     with col1:
         st.image("Profile_Pic.JPG", use_column_width=True)
     with col2:
@@ -123,6 +123,7 @@ with tab4:
         )
     except:
         st.info("Upload **team079report.pdf** to your repo to display it here.")
+
 
 
 
