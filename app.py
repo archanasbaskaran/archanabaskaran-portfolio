0694788be39bb1d4e2577e4991cf7836950ca6c6
archanasbaskaran <achusbaski@gmail.com>
2026-07-05 18:18:41 -0400

    Refactor app.py to restore original content

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ with tab1:
 
     with colB:
             st.write("""
-            **Cloud:** AWS, GCP  
+            **Cloud:** AWS, Google CLoud Platform  
             **ML/NLP:** BERT, VADER, DNN, FAISS  
             **Other:** Docker, Arena Simulation  
             """)
